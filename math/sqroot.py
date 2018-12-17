64e566aa44b3687c0ac4796cb6cde23455dff8a6
Agustin <nonsignificantp@gmail.com>
2018-12-17 03:11:11 -0300

    adds sqrRoot by iteration

--- math/sqroot.py
+++ math/sqroot.py
@@ -8,3 +8,10 @@ def sqrRoot(num, x=1):
 def sqrRoot_min(num, x=1):
     result = (x + num/x) / 2
     return result if result == x else sqrRoot(num, result)
+
+
+def sqrRoot_iter(num):
+    x = 1
+    while x != (x + num/x) / 2:
+        x = (x + num/x) / 2
+    return x
